11ebb7797d2056269bbd1dcd0eb6a022ebfc976b
Serafim Kovachevich <serafimkovacevic@gmail.com>
2026-01-31 11:37:04 +0200

    Restore person.gd to local/original version while keeping incoming for other files

--- person.gd
+++ person.gd
@@ -1,29 +1,5 @@
-extends Node2D
+extends CharacterBody2D
 
-@export var face_sprites: Array[Texture2D]
-@export var min_problem_id := 0
-@export var max_problem_id := 0
-
-var problem_id: int
-
-@onready var sprite: Sprite2D = $Sprite
-
-func _ready():
-	assign_random_problem()
-
-func assign_random_problem():
-	if face_sprites.is_empty():
-		push_error("Face sprites array is empty!")
-		return
-
-	if max_problem_id == 0:
-		max_problem_id = face_sprites.size() - 1
-
-	problem_id = randi_range(min_problem_id, max_problem_id)
-	sprite.texture = face_sprites[problem_id]
-
-func move_to(target: Vector2, duration := 0.5):
-	var tween = create_tween()
-	tween.tween_property(self, "position", target, duration)\
-		.set_trans(Tween.TRANS_SINE)\
-		.set_ease(Tween.EASE_IN_OUT)
+func _physics_process(delta: float) -> void:
+	#netanyahu
+	move_and_slide()
